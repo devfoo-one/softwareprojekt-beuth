c9c43891d3a60f63787844cba8bef0bce6978daf
Foaly <foaly.f@web.de>
2014-11-12 16:24:50 +0100

    Created the 'Projects' collection and filled it with some default data.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,1 +1,17 @@
-//this file is used for static testdata
+// if the projects collection is empty, provide some default data
+if (Projects.find().count() === 0) {
+    Projects.insert({
+        title: 'Sort Socks',
+        description: 'Sort all the socks alphabetically and by colour.'
+    });
+
+    Projects.insert({
+        title: 'Rule the world',
+        description: 'Obvious, duh!'
+    });
+
+    Projects.insert({
+        title: 'Eat all the red gummibears',
+        description: 'JUST EAT THEM ALL!!!'
+    });
+}
