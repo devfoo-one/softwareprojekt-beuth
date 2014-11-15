2ab28bdb90cb93fb3535d3b25199e23f089713bf
Tom Oberhauser <tom@devfoo.de>
2014-11-15 18:16:22 +0100

    deployed basic laika unit test

---
+++ tests/test_collection_projects.js
@@ -0,0 +1,20 @@
+//tests/posts.js
+var assert = require('assert');
+
+suite('Projects', function() {
+  test('insert new project', function(done, server) {
+    server.eval(function() {
+      Projects.insert({
+          title: 'testtitle',
+          description: 'testdescription'
+      });
+      var docs = Projects.find().fetch();
+      emit('docs', docs);
+    });
+
+    server.once('docs', function(docs) {
+      assert.equal(docs.length, 4); //4 due to the fixtures set in /server/fixtures.js
+      done();
+    });
+  });
+});
