5bbdc56e83681a5a826e4d7ad5bc5c6d3af4930d
robme91 <r.mehlitz@gmx.net>
2014-11-17 17:38:12 +0100

    Buttons added and icons set

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,19 +1,19 @@
 // if the projects collection is empty, provide some default data
 if (Projects.find().count() === 0) {
     Projects.insert({
-        projectTitle: 'Sort Socks',
+        title: 'Sort Socks',
         projectManager: 'Hans Wurst',
         description: 'Sort all the socks alphabetically and by colour.'
     });
 
     Projects.insert({
-        projectTitle: 'Rule the world',
+        title: 'Rule the world',
         projectManager: 'Klaus Kleber',
         description: 'Obvious, duh!'
     });
 
     Projects.insert({
-        projectTitle: 'Eat all the red gummibears',
+        title: 'Eat all the red gummibears',
         projectManager: 'Gundula Gause',
         description: 'JUST EAT THEM ALL!!!'
     });
