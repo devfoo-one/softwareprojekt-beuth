c139144e9a9d645b47932fb065109f61d2757fa7
Tom Oberhauser <tom@devfoo.de>
2014-11-19 21:22:40 +0100

    adding a new project via modal input dialog is working.

--- client/templates/dashboard.js
+++ client/templates/dashboard.js
@@ -5,3 +5,21 @@ Template.dashboard.helpers({
     return Projects.find();
   }
 });
+
+Template.dashboard.events({
+    'submit form': function(e) {
+        e.preventDefault();
+        var newProject = {
+            title: $(e.target).find('#titleInput').val(),
+            projectManager: $(e.target).find('#managerInput').val(),
+            description: $(e.target).find('#descriptionInput').val()
+        };
+        Projects.insert(newProject);
+        e.target.reset();
+        $('#addNewProjectModal').modal('hide');
+    },
+    'click .cancelModalButton': function(e) {
+        $(e.target).closest('.modal').modal('hide');
+    }
+
+});
