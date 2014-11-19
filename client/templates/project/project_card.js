84caf4acaaa7d03ef131daf40e6da57a9dc137e7
Tom Oberhauser <tom@devfoo.de>
2014-11-19 23:36:55 +0100

    added showWarning() method and implemented a check before a project gets deleted

--- client/templates/project/project_card.js
+++ client/templates/project/project_card.js
@@ -1,6 +1,12 @@
 /*Template Manager for Project Cards*/
 Template.projectCard.events = {
     'click .projectDeleteButton': function(){
-        Projects.remove(this._id);
+        var _this = this;
+        var onOK = function() {
+            Projects.remove(_this._id);
+        }
+        var warningHeader = "delete project '"+_this.title+"' ?";
+        var warningMessage = "do you really want to delete project '"+_this.title+"'? This cannot be undone!";
+        showWarning(warningHeader,warningMessage,onOK);
     }
 }
