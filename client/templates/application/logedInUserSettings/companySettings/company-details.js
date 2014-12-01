51c9db070f2237bcbd2d9f164ae747f3c907db88
robme91 <r.mehlitz@gmx.net>
2014-12-01 17:31:44 +0100

    Company Logo is now loaded as blob in the user collection. No Preview yet

--- client/templates/application/logedInUserSettings/companySettings/company-details.js
+++ client/templates/application/logedInUserSettings/companySettings/company-details.js
@@ -1,17 +1,26 @@
+/*Templates.companyInput.helpers({
+
+    $("#logoPreview").val() = Meteor.user().profile.companyLogo;
+});*/
+
 Template.settingsDialog.events({
-    'submit form': function(e) {
+    'submit form': function(e, template) {
         e.preventDefault();
         var companyName = $(e.target).find('#companyNameInput').val();
-        var companyLogo = $(e.target).find('#companyLogoInput').val();
+        var companyLogo = template.find('#companyLogoInput').files[0];
+        var reader = new FileReader();
         Meteor.users.update(
             {_id:Meteor.user()._id},
-            {$set:{"profile.companyName": companyName}},
-            {$set:{"profile.companyLogo": companyLogo}}
+            {$set:{"profile.companyName": companyName}}
         );
-        Meteor.users.update(
-            {_id:Meteor.user()._id},
-            {$set:{"profile.companyLogo": companyLogo}}
-        );
+        reader.onload = function(e){
+            Meteor.users.update(
+                {_id:Meteor.user()._id},
+                {$set:{"profile.companyLogo": companyLogo}}
+            );
+            $(e.target).find('img').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(companyLogo);
         e.target.reset();
         $('#setCompanyDetails').modal('hide');
     },
