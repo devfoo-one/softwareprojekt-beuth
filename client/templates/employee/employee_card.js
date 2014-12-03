7fa0b2fc2ac05d6dd00c197897e438e9e7357a23
Tom Oberhauser <tom@devfoo.de>
2014-12-03 21:44:38 +0100

    Finished the basics for add/remove employees via modal dialog. I think there´re way smoother ways to do this, but this one works.

--- client/templates/employee/employee_card.js
+++ client/templates/employee/employee_card.js
@@ -13,5 +13,12 @@ Template.employeeCard.events = {
         var warningHeader = "Delete employee '" + _this.firstName + " " + this.lastName + "' ?";
         var warningMessage = "Are you sure you want to delete the employee '" + _this.firstName + " " + this.lastName + "'? This cannot be undone!";
         showWarning(warningHeader, warningMessage, onOK, buttonLabels);
+    },
+
+    'click .employeeEditButton': function() {
+        var data = this;
+        // render Template into "Edit Employee" Modal Dialog
+        Blaze.renderWithData(Template.employeeInput, data, $("#editEmployeeFormInsert").get(0));
+        $('#editEmployeeModal').modal('show');
     }
 }
--- client/templates/employees.js
+++ client/templates/employees.js
@@ -1,13 +1,13 @@
 /*Template Manager for Employees*/
 
 Template.employees.helpers({
-  employees: function() {
-    return Employees.find();
-  }
+    employees: function() {
+        return Employees.find();
+    }
 });
 
 Template.employees.events({
-    'submit form': function(e) {
+    'submit #addNewEmployeeForm': function(e) {
         e.preventDefault();
 
         skillsInput = $(e.target).find('#skillsInput').val();
@@ -30,8 +30,44 @@ Template.employees.events({
         e.target.reset();
         $('#addNewEmployeeModal').modal('hide');
     },
+
     'click .cancelModalButton': function(e) {
         $(e.target).closest('.modal').modal('hide');
+    },
+
+    'submit #editEmployeeForm': function(e) {
+        e.preventDefault();
+        var _id = $(e.target).find('#objectID').val()
+        skillsInput = $(e.target).find('#skillsInput').val();
+        skillsArray = skillsInput.split(',');
+        //remove whitespace arround skills
+        skillsArray.forEach(function(element, index, array) {
+            array[index] = element.trim();
+        });
+        var employee = {
+            firstName: $(e.target).find('#firstNameInput').val(),
+            lastName: $(e.target).find('#lastNameInput').val(),
+            eMail: $(e.target).find('#eMailInput').val(),
+            skills: skillsArray,
+            type: $(e.target).find('#contractTypeInput').val(),
+            workTime: $(e.target).find('#workTimeInput').val()
+        };
+        Employees.update(
+            { _id: _id },
+            { $set: employee }
+        );
+        e.target.reset();
+        // remove template inserted by Blaze.renderWithData() from within an employee-card
+        // otherwise the next click on edit would render another form below the old one.
+        $('#editEmployeeFormInsert').empty();
+        $('#editEmployeeModal').modal('hide');
+    },
+
+    'click .cancelEditEmployee': function(e) {
+        $('#editEmployeeFormInsert').empty();
+        $(e.target).closest('.modal').modal('hide');
     }
+
+
 
 });
