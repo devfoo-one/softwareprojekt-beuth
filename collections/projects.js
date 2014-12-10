7fd1206bbf442148a333ed3105686bb5fb060b99
Foaly <foaly.f@web.de>
2014-12-10 17:05:50 +0100

    Brought the delete project functionality back using server-side methods.

--- collections/projects.js
+++ collections/projects.js
@@ -1,7 +1,7 @@
 Projects = new Mongo.Collection('projects');
 
 Meteor.methods({
-    // this method insert a new project into the database
+    // this method inserts a new project into the database
     createProject: function(projectAttributes) {
         var user = Meteor.user();
         
@@ -25,5 +25,18 @@ Meteor.methods({
         var projectId = Projects.insert(project);
         
         return projectId;
+    },
+    
+    // this method deletes a project from the database
+    deleteProject: function(projectId) {
+        var user = Meteor.user();
+
+        // ensure that the user is logged in
+        if (!user)
+            throw new Meteor.Error(401, "You need to be logged in to delete projects!");
+            
+        /** TODO: Maybe check if the user is the creator of the project. */
+        
+        Projects.remove(projectId);
     }
 });
