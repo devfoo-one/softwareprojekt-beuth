10b04b533dca1ef7f0fa6277e1be0b9326c24387
Foaly <foaly.f@web.de>
2014-12-10 16:58:51 +0100

    Brought the functionality to add projects back using server-side methods.

--- client/templates/dashboard.js
+++ client/templates/dashboard.js
@@ -9,16 +9,30 @@ Template.dashboard.helpers({
 Template.dashboard.events({
     'submit form': function(e) {
         e.preventDefault();
+        
+        // collect the data for the new project from the dialog
         var newProject = {
             title: $(e.target).find('#titleInput').val(),
             projectManager: $(e.target).find('#managerInput').val(),
             description: $(e.target).find('#descriptionInput').val()
         };
-        Projects.insert(newProject);
-        e.target.reset();
-        $('#addNewProjectModal').modal('hide');
+        
+        // call a method on the server to insert the new project
+        Meteor.call('createProject', newProject, function(error, id) {
+            if(error) {
+                // if an error occurs display why it happened
+                return alert(error.reason);
+            }
+
+            // if there is no error close the dialog
+            e.target.reset();
+            $('#addNewProjectModal').modal('hide');
+        });
+        
     },
+    
     'click .cancelModalButton': function(e) {
+        // cancel button was clicked, close the dialog
         $(e.target).closest('.modal').modal('hide');
     }
 });
--- collections/projects.js
+++ collections/projects.js
@@ -1,1 +1,29 @@
 Projects = new Mongo.Collection('projects');
+
+Meteor.methods({
+    // this method insert a new project into the database
+    createProject: function(projectAttributes) {
+        var user = Meteor.user();
+        
+        // ensure that the user is logged in
+        if (!user)
+            throw new Meteor.Error(401, "You need to be logged in to create new projects!");
+            
+        // ensure the project has a title
+        if (!projectAttributes.title)
+            throw new Meteor.Error(422, "Please fill in a title!");
+        
+        // create the project database entry
+        var project = {
+            title: projectAttributes.title,
+            projectManager: projectAttributes.projectManager,
+            description: projectAttributes.description,
+            creatorId: user._id
+        };
+        
+        // write the project to the database
+        var projectId = Projects.insert(project);
+        
+        return projectId;
+    }
+});
