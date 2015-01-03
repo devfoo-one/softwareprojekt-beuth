66c2ec791e8e7e52bfade5d195b10148ad574616
robme91 <r.mehlitz@gmx.net>
2015-01-04 00:32:28 +0100

    Engagements listed now. Datepicker Package added. addnewEngagement functionalitiy works now(but with default employee and project ids)

--- client/templates/employee/engagement-manager.js
+++ client/templates/employee/engagement-manager.js
@@ -2,18 +2,48 @@
 *   Template Manager for the Assign View of the Employee
 */
 
+/*helper for displaying all engagments*/
 Template.employeeDetailView.helpers({
     engagements : function(){
         return Engagements.find();
     }
 });
 
+/*helper to create the datetimepicker when the template is rendered*/
+Template.engagementInput.rendered = function () {
+    $(".engagementDatepicker").datetimepicker({
+					pickTime: false
+	});
+}
+
+
 Template.employeeDetailView.events({
-    'click #newAssignmentButton' : function(e){
-        
+    'submit #addEngagementForm' : function(e){
+        e.preventDefault();
+
+        // collect information about engagement
+        var newEngagement = {
+            projectId: 43,
+            employeeId: 44,
+            projectName: $(e.target).find('#projectNameInput').val(),
+            startDate: $(e.target).find('#startDateInput').val(),
+            endDate: $(e.target).find('#endDateInput').val()
+        };
+    
+        // call a method on the server to create the employee 
+        Meteor.call('createEngagement', newEngagement, function(error) {
+            if (error)
+                return alert(error.reason);
+
+            // reset the dialog
+            e.target.reset();
+            $('#addEngagementModal').modal('hide');
+        }); 
     },
     
     'click .cancelModalButton': function(e) {
         $(e.target).closest('.modal').modal('hide');
     }
+    
+    
 });
