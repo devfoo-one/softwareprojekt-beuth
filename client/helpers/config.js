199aaed27d37920511449ea63bcaeb75fa4bf2c8
Vicky <v.koehring@gmail.com>
2015-01-11 21:07:36 +0100

    config.js fixed #51

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,16 +1,4 @@
 Accounts.ui.config({
     passwordSignupFields: "USERNAME_AND_EMAIL_CONFIRM"
 });
-Template.dashboard.created = function() {
-    if (Accounts._verifyEmailToken) {
-        Accounts.verifyEmail(Accounts._verifyEmailToken, function(err) {
-            if (err != null) {
-                if (err.message == 'Verify email link expired [403]') {
-                    console.log('Sorry this verification link has expired.')
-                }
-            } else {
-                console.log('Thank you! Your email address has been confirmed.')
-            }
-        });
-    }
-};
+
