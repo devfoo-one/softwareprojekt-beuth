87da0ba04e057b7994c2463ab81c126af8285674
Tom Oberhauser <tom@devfoo.de>
2015-07-28 22:11:10 +0200

    Added check for duration and worktime (must not be 0 or negative value)

--- collections/employees.js
+++ collections/employees.js
@@ -4,29 +4,33 @@ Meteor.methods({
     // this method inserts a new employee into the database
     createEmployee: function(employeeAttributes) {
         var user = Meteor.user();
-        
+
         // ensure that the user is logged in
         if (!user)
             throw new Meteor.Error(401, "You need to be logged in to create new employees!");
-            
+
         // ensure the employee has a name
         if (!employeeAttributes.firstName || !employeeAttributes.lastName)
             throw new Meteor.Error(422, "Please fill in a first and last name!");
-        
+
         // create the employee database entry
         var employee = _.pick(employeeAttributes, 'firstName', 'lastName', 'eMail', 'skills', 'freeDays', 'type', 'workTime');
-        
+
+        if (employee.workTime <= 0) {
+            throw new Meteor.Error(422, "Work time must not be 0 or a negative value!");
+        }
+
         employee = _.extend(employee, {
             creatorId: user._id
         });
-        
+
         // write the employee to the database
         var employeeId = Employees.insert(employee);
-        
+
         return employeeId;
-    },    
-    
-    
+    },
+
+
     // this method deletes an employee from the database
     deleteEmployee: function(employeeId) {
         var user = Meteor.user();
@@ -34,9 +38,9 @@ Meteor.methods({
         // ensure that the user is logged in
         if (!user)
             throw new Meteor.Error(401, "You need to be logged in to delete employees!");
-            
+
         /** TODO: Maybe check if the user is the creator of the employee. */
-        
+
         Employees.remove(employeeId);
     },
 
@@ -44,20 +48,24 @@ Meteor.methods({
     // this method updates an employee in the database
     updateEmployee: function(employeeAttributes) {
         var user = Meteor.user();
-        
+
         // ensure that the user is logged in
         if (!user)
             throw new Meteor.Error(401, "You need to be logged in to edit an employee!");
-        
+
         // make sure the client passed us an Id
         if (!employeeAttributes._id)
             throw new Meteor.Error(422, "Client passed invalid data. (Id is missing)");
-            
+
         /** TODO: Make sure the user is the creator of the employee he is trying to delete. */
-            
+
         // pick only the attributes we need and create a employee database entry
         var employee = _.pick(employeeAttributes, 'firstName', 'lastName', 'eMail', 'skills', 'freeDays', 'type', 'workTime');
-        
+
+        if (employee.workTime <= 0) {
+            throw new Meteor.Error(422, "Work time must not be 0 or a negative value!");
+        }
+
         Employees.update(
             { _id: employeeAttributes._id }, // query
             { $set: employee }
